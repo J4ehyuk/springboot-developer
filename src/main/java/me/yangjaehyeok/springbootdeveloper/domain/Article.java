99371565e72bcec19e61311f9add53bec645623b
JH <yangjae0211@naver.com>
2025-05-19 22:15:59 +0900

    ~7장까지

--- src/main/java/me/yangjaehyeok/springbootdeveloper/domain/Article.java
+++ src/main/java/me/yangjaehyeok/springbootdeveloper/domain/Article.java
@@ -5,16 +5,31 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class) // 엔티티의 생성 및 수정 시간을 자동으로 감시하고 기록
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Article {
+    @CreatedDate // 엔티티가 생성될 때, 생성 시간 저장
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate // 엔티티가 수정될 때, 수정시간 저장
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 
     @Id // id 필드를 기본키로 지정
     @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본키를 자동으로 1씩 증가
     @Column(name = "id", updatable = false)
-    private  long id;
+    private long id;
+
 
     @Column(name = "title", nullable = false) // 'title'이라는 not null 컬럼과 매핑
     private String title;
---
+++ src/main/java/me/yangjaehyeok/springbootdeveloper/dto/ArticleViewResponse.java
@@ -0,0 +1,27 @@
+package me.yangjaehyeok.springbootdeveloper.dto;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.yangjaehyeok.springbootdeveloper.domain.Article;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+public class ArticleViewResponse {
+
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+
+    public ArticleViewResponse(Article article) {
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.content = article.getContent();
+        this.createdAt = article.getCreatedAt();
+    }
+
+
+}
